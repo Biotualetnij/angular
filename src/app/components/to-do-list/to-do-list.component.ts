5d7d20ebbca409e24535d65eb0d2c22b21063888
Biotualetnij <72648074+Biotualetnij@users.noreply.github.com>
2022-01-09 20:27:18 +0200

    #some fixes

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -11,17 +11,25 @@ import { FormsModule } from '@angular/forms';
 export class ToDoListComponent implements OnInit {
   self = this;
   inputValue: any;
-  usernames: string;
+  usernames: any;
+  toDoListArray: any;
 
   constructor() {
     this.usernames = 's';
+    this.toDoListArray = ['hello', 'goodbye'];
   }
 
   clickme = function (username: string) {};
+  getArrayFromLocalStorage = function () {};
 
   ngOnInit(): void {
     this.inputValue = 'hello friend';
 
+    this.getArrayFromLocalStorage = function () {
+      this.usernames = localStorage.getItem('taskArray');
+      this.toDoListArray = JSON.parse(this.usernames);
+    };
+    this.getArrayFromLocalStorage();
     this.clickme = function (username: string) {
       this.usernames = username;
       console.log('it does nothing', username);
