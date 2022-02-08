7bce4c96c23081a3bd064d070a4cce6458b022a9
Biotualetnij <72648074+Biotualetnij@users.noreply.github.com>
2022-02-08 20:07:48 +0200

    #added login charge to back

--- src/app/components/login-signup-system/login/login.component.ts
+++ src/app/components/login-signup-system/login/login.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -8,40 +7,38 @@ import { Observable } from 'rxjs';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  public user$: any;
-  public userArr = [
-    { name: 'user', password: 'userCool' },
-    { name: 'admin', password: 'admin' },
-  ];
+  public user$: { userArr: { name: string; password: string }[] } = {
+    userArr: [{ name: '', password: '' }],
+  };
   public user: any;
-  constructor(private http: HttpClient) {
-    this.user$ = [{ name: '', password: '' }];
-  }
+  constructor(private http: HttpClient) {}
+
   logIn = function (username: string, password: string) {};
 
   ngOnInit(): void {
     this.logIn = function (username: string, password: string) {
-      this.http.get<any>('assets/users.json').subscribe(
+      this.http.get<any>('http://localhost:3000/log-in/users').subscribe(
         (response) => {
-          console.log(response);
           this.user$ = response;
+
+          this.user = this.user$.userArr.find(
+            (element: { name: string; password: string }) => {
+              if (element.name == username && element.password == password) {
+                return true;
+              }
+              return false;
+            }
+          );
+          if (this.user) {
+            console.log('you are logged in');
+          } else {
+            console.log('you are not logged in');
+          }
         },
         (error) => {
           console.log(error);
         }
       );
-      // this.user = this.userArr.find((element) => {
-      //   if (element.name == username && element.password == password) {
-      //     return true;
-      //   }
-      //   return false;
-      // });
-
-      // if (this.user) {
-      //   console.log('you are logged in');
-      // } else {
-      //   console.log('you are not logged in');
-      // }
     };
   }
 }
--- src/app/components/login-signup-system/signup/signup.component.ts
+++ src/app/components/login-signup-system/signup/signup.component.ts
@@ -1,15 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  public user$: { userArr: { name: string; password: string }[] } = {
+    userArr: [{ name: '', password: '' }],
+  };
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
+  signUp = (username: string, password: string) => {};
   ngOnInit(): void {
+    this.signUp = (username: string, password: string) => {};
   }
-
 }
+// {
+//   "userArr": [
+//     { "name": "user", "password": "userCool" },
+//     { "name": "admin", "password": "admin" }
+//   ]
+// }
