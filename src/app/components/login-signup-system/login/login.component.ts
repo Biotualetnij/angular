0d7d8d64bf8e8c36501d06bb55c038d78cdf2308
Biotualetnij <72648074+Biotualetnij@users.noreply.github.com>
2022-02-08 22:10:21 +0200

    #added post request

--- src/app/components/login-signup-system/login/login.component.ts
+++ src/app/components/login-signup-system/login/login.component.ts
@@ -17,28 +17,23 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.logIn = function (username: string, password: string) {
-      this.http.get<any>('http://localhost:3000/log-in/users').subscribe(
-        (response) => {
-          this.user$ = response;
-
-          this.user = this.user$.userArr.find(
-            (element: { name: string; password: string }) => {
-              if (element.name == username && element.password == password) {
-                return true;
-              }
-              return false;
+      this.http
+        .post<any>('http://localhost:3000/log-in/checkUser', {
+          login: username,
+          password: password,
+        })
+        .subscribe(
+          (response) => {
+            if (response) {
+              console.log('you are logged in');
+            } else {
+              console.log('you are not logged in');
             }
-          );
-          if (this.user) {
-            console.log('you are logged in');
-          } else {
-            console.log('you are not logged in');
+          },
+          (error) => {
+            console.log(error);
           }
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+        );
     };
   }
 }
