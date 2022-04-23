a67ca3f374e156bb6e2f42fe6069937b947ca6b7
Dani Dan <damians.geidels2@gmail.com>
2022-04-23 20:43:01 +0300

    guard+nulltf

--- src/app/auth.guard.spec.ts
+++ src/app/auth.guard.spec.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { SignupComponent } from './components/login-signup-system/signup/signup.component'';
+import { SignupComponent } from './components/login-signup-system/signup/signup.component';
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
