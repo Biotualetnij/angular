e673c9cc294c30ff6e01b3bc0bc02e03eda182dd
Dani Dan <damians.geidels2@gmail.com>
2022-05-01 11:00:17 +0300

    d

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -7,19 +7,27 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { LoginComponent } from './components/login-signup-system/login/login.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  bsModalRef?: BsModalRef;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private modalService: BsModalService
+  ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
     if (!this.authService.isLoggedIn()) {
-      this.router.navigate(['/login']); // go to login if not authenticated
+      this.router.navigate(['']); // go to login if not authenticated
+      this.bsModalRef = this.modalService.show(LoginComponent);
       return false;
     }
     return true;
